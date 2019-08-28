d596a7c80433808d15b8d516e1717c9e008449d8
Emma Converse <emmaconverse@gmail.com>
2019-08-28 11:08:50 -0400

    start of ember tutorial tests

---
+++ tests/integration/helpers/rental-property-type-test.js
@@ -0,0 +1,36 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Helper | rental-property-type', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders correctly for a Standalone rental', async function(assert) {
+    this.set('inputValue', 'Estate');
+    await render(hbs`{{rental-property-type inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), 'Standalone');
+  });
+
+  test('it renders correctly for a Community rental', async function(assert) {
+    this.set('inputValue', 'Apartment');
+    await render(hbs`{{rental-property-type inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), 'Community');
+  });
+
+  test('it renders correctly for a Community rental', async function(assert) {
+    this.set('inputValue', 'Condo');
+    await render(hbs`{{rental-property-type inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), 'Community');
+  });
+
+  test('it renders correctly for a Community rental', async function(assert) {
+    this.set('inputValue', 'Townhouse');
+    await render(hbs`{{rental-property-type inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), 'Community');
+  });
+});
